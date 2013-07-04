451625d7c5c4427f2f561275bd9032f35aa18f36
James Ferry <jrferry@gmail.com>
2013-07-04 13:39:56 +0100

    Added pom.xml

--- MVVMBuild.Tests/UnitTest1.cs
+++ MVVMBuild.Tests/UnitTest1.cs
@@ -2,14 +2,14 @@
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 namespace MVVMBuild.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
+        [Test]
         public void TestMethod1()
         {
             int x = 1;
